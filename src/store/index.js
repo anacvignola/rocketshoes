1799b508cbd38e42397c2d9b6be242423d97bbfe
anacvignola <anacvignola@gmail.com>
2019-10-18 17:57:35 -0300

    add redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import { createStore } from 'redux';
 
-function cart() {
-  return [];
-}
+import reducer from './mudules/cart/reducer';
 
-const store = createStore(cart);
+const store = createStore(reducer);
 
 export default store;
