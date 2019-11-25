b3181ffbf370682e4b9c48007c21767ec17a5024
anacvignola <anacvignola@riseup.net>
2019-11-25 20:40:47 -0300

    using reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,40 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', 'react-hooks'],
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks'
+  ],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
     'import/prefer-default-export': 'off',
-    'no-console': ['error', { allow: ['tron'] }],
+    "no-unused-expressions": ["error", { "allowShortCircuit": true, "allowTernary": true }],
+    'no-console': ["error", {allow: ["tron"]}],
     'no-param-reassign': 'off',
-
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/state-in-constructor': 'off',
   },
 };
